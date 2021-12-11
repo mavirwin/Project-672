964a707a56473df31e65955011234bd5a1a226d5
mavirwin <maints@rit.edu>
2021-12-11 14:00:22 -0500

    Create IrwinMavis.final.r

---
+++ IrwinMavis.final.r
@@ -0,0 +1,21 @@
+print("BIOL 672 Final Project")
+
+library("ggplot2")
+library("dplyr")
+library("caret")
+library("e1071")
+library("kernlab")
+library("neuralnet")
+library("keras")
+library("tensorflow")
+
+Note1=cat("The file with")
+
+data=read.csv("https://raw.githubusercontent.com/netuohcs/BiBC_essentials_20200916/master/data/penguins_lter.csv")
+data.int=read.csv("https://raw.githubusercontent.com/mavirwin/raw-data/main/penguins_lter.integer.csv")
+#data=read.table("C:/Users/Videosystem/Desktop/RocASAsamples/Mavis_samples/archive/penguins_lter.txt", header=TRUE, sep="\t", )
+#setwd("C:/Users/Videosystem/Desktop/RocASAsamples/Mavis_samples/archive")
+getwd()
+
+#remove NA observations
+data.na.out2=na.omit(data.int)
