536515960bb549299d7adb989c77c0451fde1c64
mavirwin <maints@rit.edu>
2021-12-14 16:44:28 -0500

    fsd

--- IrwinMavis.final.r
+++ IrwinMavis.final.r
@@ -3,18 +3,43 @@ print("BIOL 672 Final Project")
 library("ggplot2")
 library("dplyr")
 library("caret")
-library("e1071")
-library("kernlab")
-library("neuralnet")
-library("keras")
-library("tensorflow")
+library("MASS")
+
+citation("MASS")
 
 Note1=cat("The file with")
 
-data=read.csv("https://github.com/mavirwin/Project-672/blob/main/ST00114_subset.csv")
-#data=read.table("C:/Users/Videosystem/Desktop/RocASAsamples/Mavis_samples/archive/penguins_lter.txt", header=TRUE, sep="\t", )
-#setwd("C:/Users/Videosystem/Desktop/RocASAsamples/Mavis_samples/archive")
+data=read.csv("https://raw.githubusercontent.com/mavirwin/Project-672/main/ST00114_subset.csv")
 getwd()
 
-#remove NA observations
-data.na.out2=na.omit(data.int)
+#factor
+Pheno=as.factor(data$Phenotype);
+
+#column groups
+Pebp1M=(cbind(data$x413741,data$x413742,data$x413745))
+Pebp1F=cbind(data$x428689,data$x428693,data$x433002)
+Idh1M=cbind(data$x190701,data$x138120,data$x138110)
+Idh1F=cbind(data$x115900,data$x115996,data$x185262)
+WTM=cbind(data$x307710,data$x348926,data$x365959,data$x410683,data$x441144,data$x483044)
+WTF=cbind(data$x417795,data$x448374,data$x296904,data$x319741,data$x349029)
+
+#mean and sd of column groups
+M.Pebp1M=rowMeans(cbind(data$x413741,data$x413742,data$x413745))
+M.Pebp1F=cbind(data$x428689,data$x428693,data$x433002)
+M.Idh1M=cbind(data$x190701,data$x138120,data$x138110)
+M.Idh1F=cbind(data$x115900,data$x115996,data$x185262)
+M.WTM=cbind(data$x307710,data$x348926,data$x365959,data$x410683,data$x441144,data$x483044)
+M.WTF=cbind(data$x417795,data$x448374,data$x296904,data$x319741,data$x349029)
+
+print(M.Pebp1M)
+
+# SD.Pebp1M=cbind(data$x413741,data$x413742,data$x413745)
+# SD.Pebp1F=cbind(data$x428689,data$x428693,data$x433002)
+# SD.Idh1M=cbind(data$x190701,data$x138120,data$x138110)
+# SD.Idh1F=cbind(data$x115900,data$x115996,data$x185262)
+# SD.WTM=cbind(data$x307710,data$x348926,data$x365959,data$x410683,data$x441144,data$x483044)
+# SD.WTF=cbind(data$x417795,data$x448374,data$x296904,data$x319741,data$x349029)
+
+dataframe= data.frame()
+
+
