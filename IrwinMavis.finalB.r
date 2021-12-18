86bc8369319f1094c01d183c2090f0d79c80c7cc
mavirwin <maints@rit.edu>
2021-12-17 20:39:21 -0700

    Update IrwinMavis.finalB.r

--- IrwinMavis.finalB.r
+++ IrwinMavis.finalB.r
@@ -11,32 +11,54 @@ Note1=cat("The file with the full dataset has multiple worksheets and is too big
           I bet we can bind columns from different tables, but in best interests of/n 
           time, I am keeping this simple by making my own datatable in excel and export it as csv.")
 
-data=read.csv("https://raw.githubusercontent.com/mavirwin/Project-672/main/ST00114_subset.csv")
+data=read.csv("https://raw.githubusercontent.com/mavirwin/Project-672/main/ST00114_subset3.csv")
 getwd()
 
-#factor
-Pheno=as.factor(data$Phenotype);
+print(data)
 
-#column groups
-Pebp1M=cbind(data$x413741,data$x413742,data$x413745)
-Pebp1F=cbind(data$x428689,data$x428693,data$x433002)
-Idh1M=cbind(data$x190701,data$x138120,data$x138110)
-Idh1F=cbind(data$x115900,data$x115996,data$x185262)
-WTM=cbind(data$x307710,data$x348926,data$x365959,data$x410683,data$x441144,data$x483044)
-WTF=cbind(data$x417795,data$x448374,data$x296904,data$x319741,data$x349029)
-
-dataframe= data.frame(Pheno,Pebp1M,Pebp1F,Idh1M,Idh1F,WTM,WTF)
-subframe= data.frame(Pebp1M,Pebp1F,Idh1M,Idh1F,WTM,WTF)
-print(subframe)
-cat("I don't think the datatable would work right.")
+# #factor
+# Pheno=as.factor(data$Phenotype);
+# 
+# #column groups
+# Pebp1M=cbind(data$x413741,data$x413742,data$x413745)
+# Pebp1F=cbind(data$x428689,data$x428693,data$x433002)
+# Idh1M=cbind(data$x190701,data$x138120,data$x138110)
+# Idh1F=cbind(data$x115900,data$x115996,data$x185262)
+# WTM=cbind(data$x307710,data$x348926,data$x365959,data$x410683,data$x441144,data$x483044)
+# WTF=cbind(data$x417795,data$x448374,data$x296904,data$x319741,data$x349029)
+# 
+# dataframe= data.frame(Pheno,Pebp1M,Pebp1F,Idh1M,Idh1F,WTM,WTF)
+# subframe= data.frame(Pebp1M,Pebp1F,Idh1M,Idh1F,WTM,WTF)
+# print(subframe)
+# cat("I doubt this type of datatable would work right.")
 
 #Renaming columns as groups then do transpose
 
-Pebp1M=rename(data$x413741,data$x413742,data$x413745)
-Pebp1F=rename(data$x428689,data$x428693,data$x433002)
-Idh1M=cbind(data$x190701,data$x138120,data$x138110)
-Idh1F=cbind(data$x115900,data$x115996,data$x185262)
-WTM=cbind(data$x307710,data$x348926,data$x365959,data$x410683,data$x441144,data$x483044)
-WTF=cbind(data$x417795,data$x448374,data$x296904,data$x319741,data$x349029)
-print(Pebp1M)
+# names(data)[c(12,13,14)] <- 'Pebp1M'
+# names(data)[9:11] <- 'Pebp1F'
+# names(data)[6:8] <- 'Idh1M'
+# names(data)[3:5] <- 'Idh1F'
+# names(data)[15:17] <- 'WTM'
+# names(data)[18:20] <- 'WTF'
+# names(data)[21:23] <- 'WTM'
+# names(data)[24:25] <- 'WTF'
+cat("Problem: the renaming process makes those I wanted the excat name was automatically adding numbers.\n
+Thus, I changed the names in excel, since I need to finishe this ASAP.")
+           
+data2=data[-c(1,2)]
+
+data3= as.data.frame(t(data2))
+head=print(data3)
+
+#mouse line groups as factor
+
+mg=as.factor(data3$Pheno.nickname)
+#Error: $ operator is invalid for atomic vectors
+
+#phenotype evaluations
+
+dataframe2 = data.frame(mg,RAS,PPI1,PPI2,PPI3,PPI4,Global)
+subframe2 = data.frame(RAS,PPI1,PPI2,PPI3,PPI4,Global)
+print (subframe2)
+
 
